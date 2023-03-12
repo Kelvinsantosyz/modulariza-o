06d3f68233167d3e643cf7e914eadb9c98a71ff0
Kelvin Santos <felipedossantoskelvin@gmail.com>
2023-03-11 21:46:25 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def soma(x, y):
+    return x + y
+
+def diminuir(x,y):
+    return x - y
+
+def multiplicacao(x,y):
+    return x * y
+
+def divisao(x,y):
+    return x / y
+
+
+mensagem= "Obrigado, voltei sempre!!"
